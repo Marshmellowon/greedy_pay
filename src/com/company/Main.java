873fb47993186ad18ae789d8f5933d7dc4a73fd0
Marshmellowon <leo503801@gmail.com>
2020-04-21 16:13:21 +0900

    fin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+class col {
+    int w;
+    int ea;
+
+    col(int a, int b) {
+        this.w = a;
+        this.ea = b;
+    }
+}
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        int coin;
+        int pay;
+        Scanner sc = new Scanner(System.in);
+        coin = sc.nextInt();
+        col[] won = new col[coin];
+        pay = sc.nextInt();
+        for (int i = 0; i < coin; i++) {
+            int w = sc.nextInt();
+            int ea = sc.nextInt();
+            won[i] = new col(w, ea);
+        }
+        int cnt = 0;
+        for (int i = 0; i < coin; i++) {
+            if (won[i].w >= pay) {
+                cnt += won[i].ea;
+            }
+        }
+        System.out.println(cnt);
     }
 }
